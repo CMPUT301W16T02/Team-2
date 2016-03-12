e88a87111807b3fea653560d4ce5d46b96cb39e6
Arshad Husain <husain3@ualberta.ca>
2016-03-11 17:38:39 -0700

    Finished sign in and create new account

--- app/src/main/java/com/example/arshadhusain/weshare/MainActivity.java
+++ app/src/main/java/com/example/arshadhusain/weshare/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.println("Hello World\n");
         setContentView(R.layout.activity_main);
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
