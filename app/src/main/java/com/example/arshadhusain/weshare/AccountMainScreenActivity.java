d3716b3407e3f21dffbd56f76ac3cb5ab5776cc1
Arshad Husain <husain3@ualberta.ca>
2016-03-12 15:37:07 -0700

    new branch of combined arshad and djuric branches

---
+++ app/src/main/java/com/example/arshadhusain/weshare/AccountMainScreenActivity.java
@@ -0,0 +1,18 @@
+package com.example.arshadhusain.weshare;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by arshadhusain on 16-03-12.
+ */
+public class AccountMainScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //this.onCreateSetup();
+        //this.onCreateListeners();
+
+    }
+
+}
