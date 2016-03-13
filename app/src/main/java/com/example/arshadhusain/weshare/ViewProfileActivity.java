22a49be86da8bc4a0d6e7be81c37496f8ac8efae
Arshad Husain <husain3@ualberta.ca>
2016-03-13 14:07:53 -0600

    added hanson's updated viewprofileactivity.java

--- app/src/main/java/com/example/arshadhusain/weshare/ViewProfileActivity.java
+++ app/src/main/java/com/example/arshadhusain/weshare/ViewProfileActivity.java
@@ -1,29 +1,77 @@
 package com.example.arshadhusain.weshare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
 public class ViewProfileActivity extends AppCompatActivity {
+    ArrayList<Account> Accounts = new ArrayList<Account>();
+    Account MyAccount;
+    String MyUsername;
+    TextView username;
+    TextView email;
+    TextView city;
+
+    public static int TEXTSIZE = 18;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
 
-        TextView username = (TextView) findViewById(R.id.username);
-        String dispUsername = "Username: " + "hardcoded guy";
-        username.setTextSize(23);
+        Intent intent = getIntent();
+
+        if(intent.hasExtra("Username")) {
+            MyUsername = intent.getStringExtra("Username");
+        }
+
+        try {
+            FileInputStream inputFile = openFileInput(MyUsername);
+            BufferedReader input = new BufferedReader(new InputStreamReader(inputFile));
+
+            Gson gson = new Gson();
+
+            Type listType = new TypeToken<ArrayList<Account>>() {
+            }.getType();
+
+            Accounts = gson.fromJson(input, listType);
+        }
+
+        catch(FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+
+        for(Account account : Accounts) {
+            if(account.getUsername().equals(MyUsername)){
+                MyAccount = account;
+            }
+        }
+
+        username = (TextView) findViewById(R.id.username);
+        String dispUsername = "Username: " + MyAccount.getUsername();
+        username.setTextSize(TEXTSIZE);
         username.setText(dispUsername);
 
-        TextView email = (TextView) findViewById(R.id.email);
-        String dispEmail = "Email: " + "hardcoded email";
-        email.setTextSize(23);
+        email = (TextView) findViewById(R.id.email);
+        String dispEmail = "Email: " + MyAccount.getEmail();
+        email.setTextSize(TEXTSIZE);
         email.setText(dispEmail);
 
-        TextView address = (TextView) findViewById(R.id.address);
-        String displAddress = "Address: " + "hardcoded address";
-        address.setTextSize(23);
-        address.setText(displAddress);
+        city = (TextView) findViewById(R.id.city);
+        String dispAddress = "Address: " + MyAccount.getCity();
+        city.setTextSize(TEXTSIZE);
+        city.setText(dispAddress);
+
     }
 }
