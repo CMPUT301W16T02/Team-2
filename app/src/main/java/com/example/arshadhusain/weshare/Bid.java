a327cac816d4b83ebe068d6930c86bf683925727
Arshad Husain <husain3@ualberta.ca>
2016-03-13 15:58:24 -0600

    merged phil's bid classes to arshad-ajuric

--- app/src/main/java/com/example/arshadhusain/weshare/Bid.java
+++ app/src/main/java/com/example/arshadhusain/weshare/Bid.java
@@ -1,30 +1,43 @@
+
 package com.example.arshadhusain.weshare;
 
 /**
  * Created by Hanson on 2016-03-05.
  */
-public class Bid {
-    private String bidder;
-    private Double amount;
 
+//Bid model class
+public class Bid {
+    private String bidder;  //User placing the bid
+    private Double amount;  //Bid amount
+    private String item;    //Item bid being place on
+
+    //Setter for bidder
     public void setBidder(String bidder) {
         this.bidder = bidder;
     }
 
+    //Getter for bidder
     public String getBidder() {
         return bidder;
     }
 
+    //Setter for amount
     public void setAmount(Double amount) {
         this.amount = amount;
     }
 
+    //Getter for amount
     public Double getAmount() {
         return amount;
     }
 
-    @Override
-    public String toString() {
-        return bidder + " | " + amount.toString() + "/h";
+    //Setter for item
+    public void setItem(String item){
+        this.item = item;
+    }
+
+    //Getter for item
+    public String getItem(){
+        return item;
     }
 }
