ec9767a42d15027ea2215aefa7826d8fb4a20a2c
Arshad Husain <husain3@ualberta.ca>
2016-03-24 16:06:42 -0600

    Added the elastic search file.

---
+++ app/src/main/java/com/example/arshadhusain/weshare/ElasticSearchAppController.java
@@ -0,0 +1,43 @@
+package com.example.arshadhusain.weshare;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.searchly.jestdroid.DroidClientConfig;
+import com.searchly.jestdroid.JestClientFactory;
+import com.searchly.jestdroid.JestDroidClient;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.searchbox.core.DocumentResult;
+import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+
+/**
+ * Created by arshadhusain on 16-03-23.
+ */
+public class ElasticSearchAppController {
+    private static JestDroidClient client;
+
+
+    public ElasticSearchAppController() {
+        this.init();
+    }
+
+
+    public void init() {
+        if(client == null) {
+            DroidClientConfig.Builder builder = new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080");
+            DroidClientConfig config = builder.build();
+
+            JestClientFactory factory = new JestClientFactory();
+            factory.setDroidClientConfig(config);
+            client = (JestDroidClient) factory.getObject();
+        }
+    }
+
+
+}
