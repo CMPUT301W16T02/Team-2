7838d6c28d9ecbf5848968299be0ffc33345bf8d
hanson-le <hanson.h.le@gmail.com>
2016-04-03 17:52:52 -0600

    ajflkdf

--- app/src/main/java/com/example/arshadhusain/weshare/SetLocationActivity.java
+++ app/src/main/java/com/example/arshadhusain/weshare/SetLocationActivity.java
@@ -1,7 +1,10 @@
 package com.example.arshadhusain.weshare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SetLocationActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class SetLocationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_location);
+
+        Button SetLocation = (Button) findViewById(R.id.SetLocation);
+
+        SetLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+
+                Intent intent = new Intent(SetLocationActivity.this, MapActivity.class); //YOU NEED HANSON'S EDIT AND VIEW PROFILE FUCNTIONALITY
+                //intent.putExtra("Username", MyUsername);
+                startActivity(intent);
+
+            }
+        });
     }
 }
