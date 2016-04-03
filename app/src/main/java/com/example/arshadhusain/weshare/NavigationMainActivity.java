edf954c15dad441e86d633545b829cf80f1f9bf7
hanson-le <hanson.h.le@gmail.com>
2016-04-03 15:08:21 -0600

    map

--- app/src/main/java/com/example/arshadhusain/weshare/NavigationMainActivity.java
+++ app/src/main/java/com/example/arshadhusain/weshare/NavigationMainActivity.java
@@ -185,10 +185,10 @@ public class NavigationMainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 setResult(RESULT_OK);
-                /*
-                Intent intent = new Intent(NavigationMainActivity.this, MainItemListActivity.class); //YOU NEED CHRIS' BORROWING FUNCTIONALITY
+
+                Intent intent = new Intent(NavigationMainActivity.this, SetLocation.class); //YOU NEED CHRIS' BORROWING FUNCTIONALITY
                 startActivity(intent);
-                */
+
             }
         });
 
