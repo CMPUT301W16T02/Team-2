06e2712f64c4d7f5b61b2cb4de7f19ff7ff35d50
hanson-le <hanson.h.le@gmail.com>
2016-04-03 08:13:10 -0600

    alfja

---
+++ app/src/main/java/com/example/arshadhusain/weshare/SetLocation.java
@@ -0,0 +1,34 @@
+package com.example.arshadhusain.weshare;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import android.location.Location;
+import android.location.LocationManager;
+
+public class SetLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_location);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+
+}
