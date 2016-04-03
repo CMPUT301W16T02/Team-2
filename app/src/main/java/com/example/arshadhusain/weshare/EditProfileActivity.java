092e8ff3c0288d5b3f43a99d8eba37be057275d7
cdjuric <djuric@ualberta.ca>
2016-04-03 15:52:40 -0600

    refactored EditProfileActivity

--- app/src/main/java/com/example/arshadhusain/weshare/EditProfileActivity.java
+++ app/src/main/java/com/example/arshadhusain/weshare/EditProfileActivity.java
@@ -8,18 +8,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
@@ -30,17 +18,16 @@ import java.util.concurrent.ExecutionException;
  * Started by NavigationMainActivity.
  * Returns to NavigationMainActivity on successful edit.
  *
- * @author Hanson
- * @version 1.0
+ * @author Hanson, Christopher
+ * @version 2.0
  */
 public class EditProfileActivity extends AppCompatActivity {
-    ArrayList<Account> Accounts = new ArrayList<Account>();
-    Account MyAccount;
-    String MyUsername;
-    TextView username;
-    EditText email;
-    EditText city;
-    Button SaveButton;
+    Account myAccount;
+    String myUsername;
+    TextView usernameText;
+    EditText emailInput;
+    EditText cityInput;
+    Button saveButton;
     public static int TEXTSIZE = 18;
     private ArrayList<Account> matchingAccounts = new ArrayList<>();
 
@@ -54,11 +41,11 @@ public class EditProfileActivity extends AppCompatActivity {
         Intent intent = getIntent();
 
         if(intent.hasExtra("myUsername")) {
-            MyUsername = intent.getStringExtra("myUsername");
+            myUsername = intent.getStringExtra("myUsername");
         }
 
         ElasticSearchAppController.GetAccountTask getAccountTask = new ElasticSearchAppController.GetAccountTask();
-        getAccountTask.execute(MyUsername);
+        getAccountTask.execute(myUsername);
         try {
             matchingAccounts.addAll(getAccountTask.get());
         } catch (InterruptedException e) {
@@ -68,79 +55,39 @@ public class EditProfileActivity extends AppCompatActivity {
         }
 
         for (Account account : matchingAccounts) {
-            if (account.getUsername().equals(MyUsername)) {
-                MyAccount = account;
+            if (account.getUsername().equals(myUsername)) {
+                myAccount = account;
             }
         }
 
-/*
-        try {
-            FileInputStream inputFile = openFileInput(MyUsername);
-            BufferedReader input = new BufferedReader(new InputStreamReader(inputFile));
-            System.out.println("File Loaded\n");
+        usernameText = (TextView) findViewById(R.id.username);
+        emailInput = (EditText) findViewById(R.id.emailField);
+        cityInput = (EditText) findViewById(R.id.cityField);
+        saveButton = (Button) findViewById(R.id.saveButton);
 
-            Gson gson = new Gson();
+        String usernameString = "Username: " + myAccount.getUsername();
+        usernameText.setTextSize(TEXTSIZE);
+        usernameText.setText(usernameString);
 
-            Type listType = new TypeToken<ArrayList<Account>>() {
-            }.getType();
+        emailInput.setText(myAccount.getEmail());
 
-            Accounts = gson.fromJson(input, listType);
-        }
+        cityInput.setText(myAccount.getCity());
 
-        catch(FileNotFoundException ex) {
-            ex.printStackTrace();
-        }
-
-        for(Account account : Accounts) {
-            if(account.getUsername().equals(MyUsername)){
-                MyAccount = account;
-            }
-        }*/
-
-        username = (TextView) findViewById(R.id.username);
-        email = (EditText) findViewById(R.id.EditProfileEmail);
-        city = (EditText) findViewById(R.id.EditProfileCity);
-        SaveButton = (Button) findViewById(R.id.SaveButton);
-
-        username.setTextSize(TEXTSIZE);
-        username.setText("Username: " + MyAccount.getUsername());
-
-        email.setText(MyAccount.getEmail());
-
-        city.setText(MyAccount.getCity());
-
-        SaveButton.setOnClickListener(new View.OnClickListener() {
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setResult(RESULT_OK);
 
-                String newEmail = email.getText().toString();
-                String newCity = city.getText().toString();
+                String newEmail = emailInput.getText().toString();
+                String newCity = cityInput.getText().toString();
 
-                MyAccount.setEmail(newEmail);
-                MyAccount.setCity(newCity);
+                myAccount.setEmail(newEmail);
+                myAccount.setCity(newCity);
 
                 //PUT REQUEST
-
                 ElasticSearchAppController.EditAccountTask editAccountTask = new ElasticSearchAppController.EditAccountTask();
-                editAccountTask.execute(MyAccount);
-/*
-                try {
-                    FileOutputStream fos = openFileOutput(MyUsername, 0);
-                    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+                editAccountTask.execute(myAccount);
 
-                    Gson gson = new Gson();
-                    gson.toJson(Accounts, out);
-
-                    out.flush();
-                    fos.close();
-                } catch (FileNotFoundException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }*/
                 finish();
             }
         });
