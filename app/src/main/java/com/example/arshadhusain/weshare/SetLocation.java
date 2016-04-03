25e474c4805f15c94767f3f853c08bcadff97e03
hanson-le <hanson.h.le@gmail.com>
2016-04-03 09:24:10 -0600

    afda

--- app/src/main/java/com/example/arshadhusain/weshare/SetLocation.java
+++ app/src/main/java/com/example/arshadhusain/weshare/SetLocation.java
@@ -1,16 +1,24 @@
 package com.example.arshadhusain.weshare;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import android.location.Location;
 import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.widget.Toast;
 
 public class SetLocation extends AppCompatActivity {
+    private static int MY_PERMISSION_ACCESS_COURSE_LOCATION = 11;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,24 @@ public class SetLocation extends AppCompatActivity {
         setContentView(R.layout.activity_set_location);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener myLocationListener =new MyLocationListener();
+
+
+        if ( ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+            Toast.makeText(getApplicationContext(), "req", Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSION_ACCESS_COURSE_LOCATION);
+        }
+
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, myLocationListener);
+        }
+        catch (SecurityException ex) {
+
+        }
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +54,38 @@ public class SetLocation extends AppCompatActivity {
             }
         });
 
+    }
+
+    class MyLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            location.getLatitude();
+            location.getLongitude();
+
+
+            String Text = "My current location is: " +
+                    "Latitud = " + location.getLatitude() +
+                    "Longitud = " + location.getLongitude();
+
+            Toast.makeText(getApplicationContext(), Text, Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
     }
 
 
