6dbdf7ab8915d7baa1898b304d9912072ab4b613
cdjuric <djuric@ualberta.ca>
2016-04-03 14:42:35 -0600

    refactored WelcomeActivity, removed SignInActivity

--- app/src/main/java/com/example/arshadhusain/weshare/WelcomeActivity.java
+++ app/src/main/java/com/example/arshadhusain/weshare/WelcomeActivity.java
@@ -10,33 +10,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 /**
- * The first activity that starts. Displays two buttons.
- * One button goes to sign in and the other creates an account.
+ * The first activity that starts. Displays username entry field, a sign in button, and a
+ * sign up button.
+ * Pressing "SIGN IN" signs the user in if their username matches an existing account.
+ * Pressing "SIGN UP" starts CreateProfileActivity.
  *
- * Started by opening the app.
- * Starts either CreateProfileActivity or SignInActivity.
+ * Started by launching the app.
  *
- * @author Arshad
- * @version 1.0
+ * @author Arshad, Hasan, Christopher
+ * @version 2.0
  */
 public class WelcomeActivity extends AppCompatActivity {
     private EditText username;
-    private boolean accountExists;
-    ArrayList<Account> Accounts = new ArrayList<Account>();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,117 +34,60 @@ public class WelcomeActivity extends AppCompatActivity {
         this.onCreateListeners();
     }
 
-
     public void onCreateSetup() {
         setContentView(R.layout.activity_welcome);
-        username = (EditText) findViewById(R.id.SignInInput1);
+        username = (EditText) findViewById(R.id.usernameField);
     }
 
+    /**
+     * Sets up the click listeners for each button.
+     */
     protected void onCreateListeners() {
 
-        Button SignInButton = (Button) findViewById(R.id.SignInButton);
-        Button mainCreateButton = (Button) findViewById(R.id.main_create_account);
+        Button signInButton = (Button) findViewById(R.id.signInButton);
+        Button signUpButton = (Button) findViewById(R.id.signUpButton);
 
-        SignInButton.setOnClickListener(new View.OnClickListener(){
+        signInButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                setResult(RESULT_OK);
-                String UserName = username.getText().toString();
+                String myUsername = username.getText().toString();
+                ArrayList<Account> matchingAccounts = new ArrayList<>();
 
+                // Retrieves a list of accounts with usernames that match the input
                 ElasticSearchAppController.GetAccountTask getAccountTask = new ElasticSearchAppController.GetAccountTask();
-                getAccountTask.execute(UserName);
-                try {
-                    Accounts.addAll(getAccountTask.get());
+                getAccountTask.execute(myUsername);
 
+                try {
+                    matchingAccounts.addAll(getAccountTask.get());
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } catch (ExecutionException e) {
                     e.printStackTrace();
                 }
 
-                if(Accounts.isEmpty())
-                {
+                // Checks for existing account with username that matches the input.
+                // Completes sign in if match found, notifies user otherwise
+                if(matchingAccounts.isEmpty()) {
                     System.out.println("no user found");
                     Toast.makeText(getApplicationContext(), "Username not found", Toast.LENGTH_LONG).show();
-                } else if (!(Accounts.isEmpty())) {
-                    Toast.makeText(getApplicationContext(), "Sign In Successful", Toast.LENGTH_LONG).show();
-
+                } else if (!(matchingAccounts.isEmpty())) {
+                    String signInMsg = "Signed in as: " + myUsername;
+                    Toast.makeText(getApplicationContext(), signInMsg, Toast.LENGTH_LONG).show();
                     Intent intent = new Intent(getApplicationContext(), NavigationMainActivity.class);
-                    intent.putExtra("Username", UserName);
-                    //startActivityForResult(intent, 1);
+                    intent.putExtra("myUsername", myUsername);
                     startActivity(intent);
-
-                    setResult(RESULT_OK);
                     finish();
-
                 }
-
-
-
-                /*try {
-                    FileInputStream inputFile = openFileInput(UserName);
-                    BufferedReader input = new BufferedReader(new InputStreamReader(inputFile));
-
-                    //String line = input.readLine();
-                    Gson gson = new Gson();
-                    //Type listType = new TypeToken<ArrayList<FuelLog>>() {}.getType();
-
-                    Type listType = new TypeToken<ArrayList<Account>>() {}.getType();
-
-                    Accounts = gson.fromJson(input, listType);
-                    accountExists = true;
-
-                    //Account account = new Account();
-
-
-
-                } catch (FileNotFoundException e) {
-                    // TODO Auto-generated catch block
-                    //e.printStackTrace();
-                    Accounts = new ArrayList<Account>();
-                    System.out.println("Username not found\n");
-                    accountExists = false;
-                    Toast.makeText(getApplicationContext(), "Username not found", Toast.LENGTH_LONG).show();
-
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    //e.printStackTrace();
-                    throw new RuntimeException();
-                }
-
-
-                //Intent intent = new Intent(SignInActivity.this, WelcomeActivity.class);
-
-                //startActivityForResult(intent, 1);
-                if (accountExists) {
-                    Toast.makeText(getApplicationContext(), "Sign In Successful", Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(getApplicationContext(), NavigationMainActivity.class);
-                    startActivity(intent);
-                    intent.putExtra("Username", UserName);
-                    startActivityForResult(intent, 1); */
-
-                    setResult(RESULT_OK);
-                    //finish();
-                //}
             }
         });
 
-        mainCreateButton.setOnClickListener(new View.OnClickListener() {
+        signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(WelcomeActivity.this, CreateProfileActivity.class);
                 startActivity(intent);
             }
         });
-
-       /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
     }
 
     /**
