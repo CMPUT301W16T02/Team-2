74fe775d60b384b95bec8cd2c83e47ade59794e2
cdjuric <djuric@ualberta.ca>
2016-04-04 12:43:03 -0600

    finished up search

--- app/src/main/java/com/example/arshadhusain/weshare/SearchResultsActivity.java
+++ app/src/main/java/com/example/arshadhusain/weshare/SearchResultsActivity.java
@@ -10,6 +10,7 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
 import java.util.regex.Pattern;
 
 /**
@@ -21,15 +22,13 @@ import java.util.regex.Pattern;
  */
 public class SearchResultsActivity extends AppCompatActivity {
 
-    private String keyword;
     private ListView searchResultsList;
-    private ArrayList<SearchResult> searchResults = new ArrayList<SearchResult>();
-    public ArrayAdapter<SearchResult> adapter;
-    private String activeUser;
-    private static int selectedItemPos;
-    private static int origItemPos;
+    private ArrayList<Item> searchResults = new ArrayList<>();
+    public ArrayAdapter<Item> adapter;
+    private String myUsername;
+    private String itemName;
+    private String itemOwner;
     static final int CHANGE_MADE = 1;
-    private ArrayList<SearchResult> keywordSearchResults = new ArrayList<SearchResult>();
     String[] individualWords;
 
 
@@ -40,84 +39,76 @@ public class SearchResultsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
 
-        if(intent.hasExtra("activeUser")) {
-            activeUser = intent.getStringExtra("activeUser");
+        if(intent.hasExtra("myUsername")) {
+            myUsername = intent.getStringExtra("myUsername");
         }
         if(intent.hasExtra("stringOfWords")) {
             individualWords = intent.getStringArrayExtra("stringOfWords");
         }
-
 
         searchResultsList = (ListView) findViewById(R.id.searchResultsList);
 
         searchResultsList.setOnItemClickListener(onItemClickListener);
     }
 
-    public ArrayList<SearchResult> getSearchResults(){
-        ArrayList<SearchResult> searchResults = new ArrayList<SearchResult>();
+    public ArrayList<Item> getSearchResults(){
+        searchResults.clear();
 
-        /*
-        switch (keyword) {
-            case "":
-                for (int i = 0; i < NavigationMainActivity.allItems.size(); i++) {
-                    if (NavigationMainActivity.allItems.get(i).getStatus() != 2) {
-                        searchResults.add(NavigationMainActivity.allItems.get(i));
-                    }
-                }
-            default:
-                for(int i=0; i<NavigationMainActivity.allItems.size(); i++){
-                    if(NavigationMainActivity.allItems.get(i).getStatus()!=2 &&
-                            NavigationMainActivity.allItems.get(i).getDescription().toLowerCase().contains(keyword)){
-                        searchResults.add(NavigationMainActivity.allItems.get(i));
-                    }
-                }
+        ElasticSearchAppController.GetMyItemsTask getMyItemsTask = new ElasticSearchAppController.GetMyItemsTask();
+        getMyItemsTask.execute("");
+        ArrayList<Item> allItems = new ArrayList<>();
+        try {
+            allItems.addAll(getMyItemsTask.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
-        */
 
-        for(int i=0; i<NavigationMainActivity.allItems.size(); i++){
-            int itemStatus = NavigationMainActivity.allItems.get(i).getStatus();
-            String itemOwner = NavigationMainActivity.allItems.get(i).getOwner();
-            if(itemStatus!=2 && !itemOwner.equals(activeUser)){
-                SearchResult newResult = new SearchResult(NavigationMainActivity.allItems.get(i), i);
-                searchResults.add(newResult);
+        for (Item item : allItems) {
+            if (item.getStatus() != 2 && !item.getOwner().equals(myUsername)) {
+                searchResults.add(item);
             }
         }
         return searchResults;
     }
 
-    public ArrayList<SearchResult> getKeywordSearchResults() {
+        public ArrayList<Item> getKeywordSearchResults() {
         searchResults.clear();
-        ArrayList<SearchResult> keywordSearchResults = new ArrayList<SearchResult>();
-        for(int i=0; i<NavigationMainActivity.allItems.size(); i++){
-            int itemStatus = NavigationMainActivity.allItems.get(i).getStatus();
-            String itemOwner = NavigationMainActivity.allItems.get(i).getOwner();
+
+        ElasticSearchAppController.GetMyItemsTask getMyItemsTask = new ElasticSearchAppController.GetMyItemsTask();
+        getMyItemsTask.execute("");
+        ArrayList<Item> allItems = new ArrayList<>();
+        try {
+            allItems.addAll(getMyItemsTask.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        for (Item item : allItems) {
+            System.out.printf("in the for loop!\n");
+            System.out.printf(item.getStatus().toString() + "\n" + item.getOwner() + "\n");
             boolean containsWord = true;
-            if(itemStatus!=2 && !itemOwner.equals(activeUser)){
-                //NavigationMainActivity.allItems.get(1).getOwner().con
-                //add CONTAINS here
+            if (item.getStatus() != 2 && !item.getOwner().equals(myUsername)) {
+                System.out.printf("in the if loop! status != 2 and username matches\n");
                 int keywordIterator = 0;
-                System.out.println(NavigationMainActivity.allItems.get(i).getDescription());
-
-                while(containsWord == true)
-                {
-                    containsWord = NavigationMainActivity.allItems.get(i).getDescription().toString().toLowerCase().contains(individualWords[keywordIterator].toLowerCase());
-                    //containsWord = Pattern.compile(Pattern.quote(NavigationMainActivity.allItems.get(i).getDescription().toString()), Pattern.CASE_INSENSITIVE).matcher(individualWords[keywordIterator]).find();
-
-                    System.out.println(containsWord);
-                    if(keywordIterator == individualWords.length -1)
-                    {
+                while(containsWord == true) {
+                    System.out.printf("in the while loop!\n");
+                    containsWord = item.getDescription().toString().toLowerCase().contains(individualWords[keywordIterator].toLowerCase());
+                    if (keywordIterator == individualWords.length - 1) {
+                        System.out.printf("in the second if loop!\n");
                         break;
                     }
                     keywordIterator++;
                 }
                 if (containsWord == true) {
-                    System.out.println("****INSIDE ADDING SEARCH RESULT WORDS****");
-                    SearchResult newResult = new SearchResult(NavigationMainActivity.allItems.get(i), i);
-                    searchResults.add(newResult);
+                    searchResults.add(item);
                 }
-
             }
         }
+
         return searchResults;
 
     }
@@ -128,52 +119,26 @@ public class SearchResultsActivity extends AppCompatActivity {
     private AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectedItemPos = position;
-            origItemPos = searchResults.get(selectedItemPos).getOrigItemPos();
-            viewItemInfo();
-            /*
-            if (NavigationMainActivity.allItems.get(origItemPos).getOwner().equals(activeUser)){
-                editItem();
-            } else {
-                viewItemInfo();
-            }*/
+            itemName = searchResults.get(position).getName();
+            itemOwner = searchResults.get(position).getOwner();
+            viewItemInfo(itemName, itemOwner);
         }
     };
 
     /**
-     * @deprecated
-     */
-    public void editItem() {
-        Intent intent = new Intent(this, EditItemActivity.class);
-        intent.putExtra("itemPos", origItemPos);
-
-        startActivityForResult(intent, CHANGE_MADE);
-    }
-
-    /**
      * User sees item from search
      */
-    public void viewItemInfo(){
+    public void viewItemInfo(String itemName, String ownerName){
         Intent intent = new Intent(this, ItemInfoActivity.class);
-        intent.putExtra("itemPos", origItemPos);
-        intent.putExtra("activeUser", activeUser);
-
+        intent.putExtra("itemName", itemName);
+        intent.putExtra("ownerName", ownerName);
+        intent.putExtra("myUsername", myUsername);
         startActivityForResult(intent, CHANGE_MADE);
     }
-
     @Override
     protected void onStart() {
         super.onStart();
 
-        /*
-        Intent intent = getIntent();
-
-        if(intent.hasExtra("keyword")) {
-            keyword = intent.getStringExtra("keyword").toLowerCase();
-        } else {
-            keyword = "";
-        }
-        */
         if(individualWords != null)
         {
             searchResults = getKeywordSearchResults();
@@ -184,9 +149,7 @@ public class SearchResultsActivity extends AppCompatActivity {
             System.out.println("****NO SEARCH KEYWORDS****");
         }
 
-
-        adapter = new ArrayAdapter<SearchResult>(this,
-                R.layout.list_item, searchResults);
+        adapter = new ArrayAdapter<Item>(this, R.layout.list_item, searchResults);
         searchResultsList.setAdapter(adapter);
     }
 }
