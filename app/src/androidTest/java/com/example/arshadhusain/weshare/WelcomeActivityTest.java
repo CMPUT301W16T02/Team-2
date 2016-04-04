129e89882261974e15df8385b1c820d04dffe3cc
badran047 <hasan_badran@windowslive.com>
2016-04-04 15:48:24 -0600

    fixed WelcomeActivityTest

--- app/src/androidTest/java/com/example/arshadhusain/weshare/WelcomeActivityTest.java
+++ app/src/androidTest/java/com/example/arshadhusain/weshare/WelcomeActivityTest.java
@@ -1,8 +1,9 @@
 package com.example.arshadhusain.weshare;
 
-import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
-
+import android.test.TouchUtils;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -10,11 +11,63 @@ import android.test.ActivityInstrumentationTestCase2;
  */
 public class WelcomeActivityTest extends ActivityInstrumentationTestCase2 {
     public WelcomeActivityTest(){
-        super(com.example.arshadhusain.weshare.WelcomeActivity.class);
+        super(WelcomeActivity.class);
     }
 
-    public void testStart() throws Exception{
-        Activity activity = getActivity();
+    public void testActivityExists() {
+        WelcomeActivity activity = (WelcomeActivity) getActivity();
+        assertNotNull(activity);
     }
 
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+
+    // taken from:
+    //http://evgenii.com/blog/testing-activity-in-android-studio-tutorial/
+    
+    public void testWelcomeActivity() throws Exception {
+        WelcomeActivity activity = (WelcomeActivity) getActivity();
+
+
+        //Type username in text input
+        // ----------------------
+        final EditText nameEditText =
+                (EditText) activity.findViewById(R.id.usernameField);
+
+        // Send string input value
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                nameEditText.requestFocus();
+            }
+        });
+
+        getInstrumentation().waitForIdleSync();
+        getInstrumentation().sendStringSync("badran047");
+        getInstrumentation().waitForIdleSync();
+
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                nameEditText.requestFocus();
+            }
+        });
+
+        getInstrumentation().waitForIdleSync();
+        getInstrumentation().sendStringSync("badran047");
+
+        // Tap "SIGN IN" button
+        // ----------------------
+
+        Button signInButton = (Button) activity.findViewById(R.id.signInButton);
+        TouchUtils.clickView(this, signInButton);
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
 }
