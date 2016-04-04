5c5d671062e055efbcf56140d0220b3f0c470396
phlam1 <phlam1@ualberta.ca>
2016-04-04 13:11:17 -0600

    Start test cases

--- app/src/androidTest/java/com/example/arshadhusain/weshare/EditProfileActivityTest.java
+++ app/src/androidTest/java/com/example/arshadhusain/weshare/EditProfileActivityTest.java
@@ -2,6 +2,8 @@ package com.example.arshadhusain.weshare;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import java.util.ArrayList;
+
 /**
  * Created by Overseer on 2016-03-14.
  */
@@ -14,5 +16,16 @@ public class EditProfileActivityTest extends ActivityInstrumentationTestCase2 {
     @Override
     public void setUp() throws Exception {
         super.setUp();
+    }
+
+    public void testUpdateProfile() {
+        String myUsername = "Jimmy";
+        String myCity = "Edmonton";
+        String myEmail = "boygenius@hotmail.om";
+
+        Account fakeAccount = new Account(myUsername, myCity, myEmail);
+        assertEquals(fakeAccount.getUsername(), "Jimmy");
+        assertEquals(fakeAccount.getCity(), "Edmonton");
+        assertEquals(fakeAccount.getEmail(), "boygenius@hotmail.com");
     }
 }
